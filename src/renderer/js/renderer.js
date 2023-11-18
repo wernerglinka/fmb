7c3ecd3c414a8baa8b47ba3da5607347bdc0b73b
Werner Glinka <werner@glinka.co>
2023-11-18 16:15:18 -0600

    editor integration wip

--- src/renderer/js/renderer.js
+++ src/renderer/js/renderer.js
@@ -396,6 +396,41 @@ function createComponent(type) {
       document.body.appendChild(editorOverlay);
       // attach the easyMDEditor to the textarea
       new EasyMDE({element: easyMDEditor});
+
+      // add a close button
+      const closeButton = document.createElement('div');
+      closeButton.id = "closeEditor";
+      closeButton.innerHTML = `
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <g stroke="#ffffff" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"> 
+            <circle cx="11" cy="11" r="10"></circle>
+            <line x1="14" y1="8" x2="8" y2="14"></line>
+            <line x1="8" y1="8" x2="14" y2="14"></line>
+          </g>
+        </svg>
+      `;
+      editorOverlay.appendChild(closeButton);
+
+      // add a button to the easyMDEitor to disable the inline markdown styles
+      const disableMarkdownStyles = document.createElement('button');
+      disableMarkdownStyles.id = "disableMarkdownStyles";
+      disableMarkdownStyles.innerHTML = "Inline Styles";
+      // add the button to the toolbar
+      const toolbar = document.querySelector('.editor-toolbar');
+      toolbar.appendChild(disableMarkdownStyles);
+
+      // add eventlistener to the close button
+      closeButton.addEventListener('click', () => {
+        editorOverlay.classList.remove('show');
+      });
+
+      // add eventlistener to the disableMarkdownStyles button
+      disableMarkdownStyles.addEventListener('click', (event) => {
+        event.target.classList.toggle('disabled');
+        const codemirrorWrapper = document.querySelector('.CodeMirror');
+        codemirrorWrapper.classList.toggle('disable-markdown-styles');
+      });
+
     }
 
   }
